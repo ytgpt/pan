3a30147b160a1e578c32b6fb2825aa093dd4da41
ytgpt <133744127+ytgpt@users.noreply.github.com>
2023-12-01 14:05:10 +0800

    Update pan.sh

--- pan.sh
+++ pan.sh
@@ -1,14 +1,42 @@
+#!/bin/bash
+
 REPO="pandora-next/deploy"
 API_URL="https://api.github.com/repos/$REPO/releases"
 
-latest_release=$(curl -s $API_URL | grep -m 1 "browser_download_url.*amd64" | cut -d '"' -f 4)
+latest_release=$(curl -s "$API_URL" | grep -m 1 "browser_download_url.*amd64" | cut -d '"' -f 4)
 
-file_name=$(echo $latest_release | awk -F/ '{print $NF}')
+if [ -z "$latest_release" ]; then
+    echo "Error: Failed to fetch the latest release URL"
+    exit 1
+fi
 
-curl -LJO $latest_release
+file_name=$(basename "$latest_release")
 
-tar -zxvf "$file_name" --overwrite
+curl -LO "$latest_release"
 
-mv -f ${file_name%.tar.gz} p
+if [ ! -f "$file_name" ]; then
+    echo "Error: Failed to download the file"
+    exit 1
+fi
 
-rm "$file_name"
+# Extracting the contents to a temp directory
+temp_dir=$(mktemp -d)
+tar -zxvf "$file_name" -C "$temp_dir" --strip-components=1
+
+if [ $? -ne 0 ]; then
+    echo "Error: Failed to extract the file"
+    exit 1
+fi
+
+# Renaming the directory to 'p'
+mv -f "$temp_dir" "p"
+
+if [ $? -ne 0 ]; then
+    echo "Error: Failed to rename directory"
+    exit 1
+fi
+
+# Removing the downloaded file
+rm -f "$file_name"
+
+echo "Script execution completed successfully"
